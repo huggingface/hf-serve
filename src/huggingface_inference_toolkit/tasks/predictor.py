2652b6622892579f085059ca60b462836b821530
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-02-18 17:17:28 +0100

    Add handling for `Union` of different schemas i.e. `anyOf`

--- src/huggingface_inference_toolkit/tasks/predictor.py
+++ src/huggingface_inference_toolkit/tasks/predictor.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Union
 
 from pydantic import BaseModel
 
-InputType = TypeVar("InputType", bound=BaseModel)
-OutputType = TypeVar("OutputType", bound=BaseModel)
+# NOTE: here to handle both the standard type and the `anyOf` syntax if multiple I/O schemas are valid
+InputType = TypeVar("InputType", bound=Union[BaseModel, Union[BaseModel, ...]])  # type: ignore
+OutputType = TypeVar("OutputType", bound=Union[BaseModel, Union[BaseModel, ...]])  # type: ignore
 
 
 class Predictor(ABC, Generic[InputType, OutputType]):
