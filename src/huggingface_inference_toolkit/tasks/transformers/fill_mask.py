cf3622a3e9f742ef90d610529c7d501c0665f50f
erikkaum <erik.kaum@gmail.com>
2025-02-18 16:36:21 +0100

    run ruff format

--- src/huggingface_inference_toolkit/tasks/transformers/fill_mask.py
+++ src/huggingface_inference_toolkit/tasks/transformers/fill_mask.py
@@ -17,6 +17,7 @@ class FillMaskInput(BaseModel):
     )
     parameters: Optional[FillMaskParameters] = None
 
+<<<<<<< HEAD
     model_config = ConfigDict(
         json_schema_extra={
             "examples": [
@@ -30,6 +31,8 @@ class FillMaskInput(BaseModel):
         }
     )
 
+=======
+>>>>>>> 939a4c1 (run ruff format)
 
 class FillMaskOutputValue(BaseModel):
     score: float
@@ -81,3 +84,12 @@ class FillMask(Predictor[FillMaskInput, FillMaskOutput]):
 
         pipeline_results = self.pipeline(**payload)  # type: ignore
         return FillMaskOutput(root=pipeline_results)
+<<<<<<< HEAD
+=======
+
+    def _example(self) -> FillMaskInput:
+        return FillMaskInput(
+            inputs="Mona Lisa is located in the [MASK], which is where I was it for the first time",
+            parameters=FillMaskParameters(top_k=3),
+        )
+>>>>>>> 939a4c1 (run ruff format)
