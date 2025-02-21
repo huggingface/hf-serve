faed66c8af61e5dd33f288e405a9b37284494e89
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-02-21 12:07:41 +0100

    Align `logging` with previous logging messages
    Everything's aligned besides the `uvicorn` logging messages that have
    been kept

--- src/huggingface_inference_toolkit/logging.py
+++ src/huggingface_inference_toolkit/logging.py
@@ -1,4 +1,10 @@
 import logging
+import sys
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    stream=sys.stdout,
+)
+logger = logging.getLogger("huggingface-inference-toolkit")
