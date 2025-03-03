4b30e848841b77cba2f1a47755cb44adbf7ef6b0
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-03-03 16:09:42 +0100

    Add support for custom tasks via `EndpointHandler` (and `PreTrainedPipeline`)
    Both formats should eventually be deprecated in favor of pydantic-based
    `Predictor` instances as of the refactor, but those deprecated formats
    will still be there for backwards compatibility in the meantime

--- src/huggingface_inference_toolkit/backwards.py
+++ src/huggingface_inference_toolkit/backwards.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 
 from huggingface_inference_toolkit.logging import logger
 
@@ -11,6 +10,8 @@ def check_backwards_compatibility() -> None:
         "HF_TASK": "TASK",
         "HF_REVISION": "REVISION",
         "HF_HUB_TOKEN": "HF_TOKEN",
+        "HF_MODULE_NAME": "CUSTOM_HANDLER",
+        "HF_DEFAULT_PIPELINE_NAME": "CUSTOM_HANDLER_FILE",
         # TODO: this one most likely should be deprecated, IIRC Google needed it, but it
         # shouldn't be allowed publicly in Inference Endpoints
         # "HF_TRUST_REMOTE_CODE": "TRUST_REMOTE_CODE",
@@ -30,7 +31,13 @@ def check_backwards_compatibility() -> None:
         )
         os.environ["FRAMEWORK"] = "torch"
 
-    # TODO: "HF_DEFAULT_PIPELINE_NAME"
+    # NOTE: for the custom handler we need to set those values by default if not provided, otherwise,
+    # the provided values will be used
+    if "CUSTOM_HANDLER_FILE" not in os.environ:
+        os.environ["CUSTOM_HANDLER_FILE"] = "handler.py"
+
     if "CUSTOM_HANDLER" not in os.environ:
-        handler_file = os.environ.get("CUSTOM_HANDLER_FILE", "handler.py")
-        os.environ["CUSTOM_HANDLER"] = f"{Path(handler_file).stem}.EndpointHandler"
+        # NOTE: this value used to be provided as the full path to the `EndpointHandler` or whatever name it
+        # had, but since the `CUSTOM_HANDLER_FILE` is a single file, it doesn't make much sense to provided the
+        # stem path i.e. `handler.EndpointHandler`, as that will be automatically defined internally already
+        os.environ["CUSTOM_HANDLER"] = "EndpointHandler"
