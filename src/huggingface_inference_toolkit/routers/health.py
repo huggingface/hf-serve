6a0695302752b1af2aa36741d5cc46d30d234b6c
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-03-11 13:44:00 +0100

    Add note on alt `health` response

--- src/huggingface_inference_toolkit/routers/health.py
+++ src/huggingface_inference_toolkit/routers/health.py
@@ -7,3 +7,12 @@ router = APIRouter()
 @router.get("/health")
 def health() -> JSONResponse:
     return JSONResponse(content={"status": "ok"}, status_code=200)
+
+
+# NOTE: in case returning a JSON instead of a plain text response is not compliant with what the Inference Endpoints
+# expect, then use the implementation below instead
+# from fastapi.responses import PlainTextResponse
+#
+# @router.get("/health", response_class=PlainTextResponse)
+# def health() -> PlainTextResponse:
+#     return "ok"
