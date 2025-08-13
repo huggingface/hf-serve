06e7a430f2cee5b207b82d565d6122f27b044fe4
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-08-13 18:33:10 +0200

    Add `/score` route redirecting to `/predict` for Azure (#21)
    * Add `/score` route with redirect to `/predict`
    
    * Set `request` to `_` due to unused arg

--- src/huggingface_inference_toolkit/clouds/azure.py
+++ src/huggingface_inference_toolkit/clouds/azure.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 
 router = APIRouter()
 
@@ -7,3 +7,8 @@ router = APIRouter()
 @router.get("/swagger.json")
 async def swagger_json(request: Request) -> JSONResponse:
     return request.app.openapi()
+
+
+@router.post("/score")
+async def score(_: Request):
+    return RedirectResponse(url="/predict", status_code=307)
