074d67e95a4c5c1b7dddba26b8adc292d1a6bd70
juanjucm <jjceamoran@gmail.com>
2025-08-19 10:15:10 +0200

    using load_image from transformers

--- src/huggingface_inference_toolkit/serde/image.py
+++ src/huggingface_inference_toolkit/serde/image.py
@@ -1,9 +1,9 @@
 import base64
 from io import BytesIO
-import requests
 from typing import Union
 
 from PIL import Image as PILImage
+from transformers.image_utils import load_image
 
 
 class Image:
@@ -13,15 +13,7 @@ class Image:
             if isinstance(image_input, bytes):
                 return PILImage.open(BytesIO(image_input))
             elif isinstance(image_input, str):
-                if image_input.startswith(("http://", "https://")):
-                    response = requests.get(image_input)
-                    response.raise_for_status()
-                    return PILImage.open(BytesIO(response.content))
-                elif "." in image_input.split("/")[-1]:
-                    # Assume it's a file path
-                    return PILImage.open(image_input)
-                else:
-                    return PILImage.open(BytesIO(base64.b64decode(image_input)))
+                return load_image(image_input)
             else:
                 raise ValueError("Unsupported image input type")
         except Exception as e:
