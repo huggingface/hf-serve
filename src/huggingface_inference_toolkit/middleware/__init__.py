41d9ded662fc49a2a5ad8d5f719945d11ab24898
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-08-29 12:18:31 +0200

    Add `RequestIdMiddleware` to identify the requests by ID

--- src/huggingface_inference_toolkit/middleware/__init__.py
+++ src/huggingface_inference_toolkit/middleware/__init__.py
@@ -1,4 +1,5 @@
 from huggingface_inference_toolkit.middleware.logging import LoggingMiddleware
 from huggingface_inference_toolkit.middleware.prometheus import PrometheusMiddleware
+from huggingface_inference_toolkit.middleware.request_id import RequestIdMiddleware
 
-__all__ = ["LoggingMiddleware", "PrometheusMiddleware"]
+__all__ = ["LoggingMiddleware", "PrometheusMiddleware", "RequestIdMiddleware"]
