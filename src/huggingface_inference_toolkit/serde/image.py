26199533e99a2a9cddfb84ace7a570051fa53d5e
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-08-29 12:19:29 +0200

    Fix `Image` type in `serde/image.py`

--- src/huggingface_inference_toolkit/serde/image.py
+++ src/huggingface_inference_toolkit/serde/image.py
@@ -2,26 +2,25 @@ import base64
 from io import BytesIO
 from typing import Union
 
-from PIL import Image as PILImage
+from PIL import Image as ImageModule
+from PIL.Image import Image as ImageType
 from transformers.image_utils import load_image
 
 
 class Image:
     @staticmethod
-    def deserialize(image_input: Union[str, bytes]) -> PILImage.Image:
+    def deserialize(image_input: Union[str, bytes]) -> ImageType:
         try:
             if isinstance(image_input, bytes):
-                return PILImage.open(BytesIO(image_input))
+                return ImageModule.open(BytesIO(image_input))
             elif isinstance(image_input, str):
                 img = load_image(image_input)
                 return img
-            else:
-                raise ValueError("Unsupported image input type")
         except Exception as e:
             raise ValueError(f"Failed to deserialize image: {e}")
 
     @staticmethod
-    def serialize(image: PILImage) -> str:
+    def serialize(image: ImageType) -> str:
         buffered = BytesIO()
         image.save(buffered, format="PNG")
         return base64.b64encode(buffered.getvalue()).decode()
