b5369a6b781c851452f6545763c1221444e8c94d
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-09-16 12:55:22 +0200

    Use `Audio.deserialize` in `AudioClassification`

--- src/hf_serve/tasks/transformers/audio_classification.py
+++ src/hf_serve/tasks/transformers/audio_classification.py
@@ -80,12 +80,12 @@ class AudioClassification(Predictor[AudioClassificationInput, AudioClassificatio
         if payload.parameters:
             parameters = payload.parameters.model_dump(exclude_none=True)
 
-        audio = payload.inputs
-        if isinstance(audio, str):
-            # NOTE: Deserialize it into `bytes` if it's a base64-encoded `str`, or leave it as is if it's either
-            # a URL or a filepath as it will be automatically handled by the `AutoPipeline.__call__`
-            if not audio.startswith(("/", "http://", "https://")) and "." not in audio.split("/")[-1]:
-                audio = Audio.deserialize(audio)
+        # NOTE: Handle different input types: bytes, URL, file path, or base64; no need to handle others given that
+        # `Pydantic` already validates that the `inputs` is either `bytes` or `str`
+        if isinstance(payload.inputs, bytes):
+            audio_bytes = payload.inputs
+        elif isinstance(payload.inputs, str):
+            audio_bytes = Audio.deserialize(payload.inputs)
 
-        results = self.pipeline(audio, **parameters)
+        results = self.pipeline(audio_bytes, **parameters)  # type: ignore
         return AudioClassificationOutput(results=results)  # type: ignore
