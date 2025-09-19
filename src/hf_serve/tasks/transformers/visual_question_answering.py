1e48dde833f06b5c3d7f013ef86a948d584ea620
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-09-19 11:43:33 +0200

    Fix output for `visual-question-answering`

--- src/hf_serve/tasks/transformers/visual_question_answering.py
+++ src/hf_serve/tasks/transformers/visual_question_answering.py
@@ -38,9 +38,7 @@ class VisualQuestionAnsweringInput(BaseModel):
 
 class VisualQuestionAnsweringOutputValue(BaseModel):
     # NOTE: Transformers documentation says this should be `label` but apparently it's `answer` instead
-    # for `image_processor.is_vqa`
-    # answer: str
-    label: str
+    answer: str
 
     # NOTE: `score` should be optional if `image_processor.is_vqa` was supported, given that's not the
     # case for the moment, it's left as mandatory
