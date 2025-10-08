f5b37db73460f1d61eef4f389bd200e885a59ae1
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-10-08 11:34:12 +0200

    Pop unsupported `parameters` for `text-generation` and `image-text-to-text`

--- src/hf_serve/tasks/transformers/text_generation.py
+++ src/hf_serve/tasks/transformers/text_generation.py
@@ -1,25 +1,37 @@
-from typing import Optional
+from typing import Any, Optional
 
-from pydantic import AliasChoices, AliasPath, BaseModel, Field
+from pydantic import AliasChoices, AliasPath, BaseModel, Field, model_validator
 
+from hf_serve.logging import logger
 from hf_serve.tasks.predictor import Predictor
 
 
 class TextGenerationParameters(BaseModel):
-    adapter_id: Optional[str] = Field(default=None)
     do_sample: Optional[bool] = Field(default=True)
-    grammar: Optional[str] = Field(default=None)
     max_new_tokens: Optional[int] = Field(default=20)
     repetition_penalty: Optional[float] = Field(default=1.0)
     return_full_text: Optional[bool] = Field(default=False)
     seed: Optional[int] = Field(default=None)
-    stop: Optional[list[str]] = Field(default=None)
     temperature: Optional[float] = Field(default=1.0)
     top_k: Optional[int] = Field(default=None)
-    top_n_tokens: Optional[int] = Field(default=None)
     top_p: Optional[float] = Field(default=1.0)
-    truncate: Optional[int] = Field(default=None)
     typical_p: Optional[float] = Field(default=1.0)
+
+    # NOTE: All the parameters below are defined within the Inference API Specification but not supported within
+    # `hf-serve`, hence allowing those but raising a warning if those are provided
+    adapter_id: Optional[str] = Field(default=None)
+    grammar: Optional[str] = Field(default=None)
+    stop: Optional[list[str]] = Field(default=None)
+    top_n_tokens: Optional[int] = Field(default=None)
+    truncate: Optional[int] = Field(default=None)
+
+    @model_validator(mode="after")
+    def validate_unsupported_params(self):
+        if any(getattr(self, p) for p in {"adapter_id", "grammar", "stop", "top_n_tokens", "truncate"}):
+            logger.warning(
+                "Unsupported parameters will be ignored: adapter_id, grammar, stop, top_n_tokens, truncate"
+            )
+        return self
 
 
 class TextGenerationInput(BaseModel):
@@ -60,6 +72,11 @@ class TextGeneration(Predictor[TextGenerationInput, TextGenerationOutput]):
             from transformers import set_seed
 
             set_seed(seed)
+
+        # NOTE: Removing these here intead of within the schema as otherwise when logging the schema the user
+        # might be confused if they see that the schema is different to what they provided despite the warning
+        for p in {"adapter_id", "grammar", "stop", "top_n_tokens", "truncate"}:
+            parameters.pop(p, None)
 
         output = self.pipeline(text_inputs=payload.inputs, **parameters)
         generated_text = output[0]["generated_text"]
